294c2b719b047f77c589658a857f364f9a972017
Ben Wallberg <wallberg@umd.edu>
2014-11-17 15:27:30 -0500

    change transform.py to export.py
    lxml doesn't support XSLT 2.0 so remove the transform code and extract
    to descMeta collection output

--- export.py
+++ export.py
@@ -7,7 +7,7 @@ from urllib.parse import urlencode
 
 # Adapted from http://stackoverflow.com/questions/9809469/python-sax-to-lxml-for-80gb-xml/9814580#9814580
 
-# Example program to read a large Solr XML search result, parse out the UMDM, transform to MODS, and write to sys.stdout
+# Example program to read a large Solr XML search result, parse out the UMDM, and write to sys.stdout
 
 # command-line arguments
 (baseUrl, query, rows) = sys.argv[1:]
@@ -16,11 +16,8 @@ from urllib.parse import urlencode
 params = urlencode({'q':query, 'rows':rows, 'fl':'umdm','wt':'xml'})
 url = "{}?{}".format(baseUrl, params)
 
-# setup the XSLT transformer
-transform = ET.XSLT(ET.parse('test.xsl'))
-
 sys.stdout.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
-sys.stdout.write("<mods:modsCollection xmlns:mods=\"http://www.loc.gov/mods/v3\">\n")
+sys.stdout.write("<collection>\n")
 
 # user iterparse to stream in each UMDM w/o filling up memory
 context = ET.iterparse(urlopen(url), events=('end',))
@@ -30,13 +27,10 @@ for action, elem in context:
         # the UMDM is encoded as text so decode and parse to XML
         descMeta = ET.fromstring(elem.text)
 
-        # XSLT transform the single descMeta to mods
-        mods = transform(descMeta)
-
         # write the single mods record to sts.stdout
-        modsBytes = ET.tostring(mods)
-        if (modsBytes):
-            sys.stdout.write(modsBytes.decode("utf-8"))
+        descMetaBytes = ET.tostring(descMeta)
+        if (descMetaBytes):
+            sys.stdout.write(descMetaBytes.decode("utf-8"))
             sys.stdout.write('\n')
 
     # cleanup
@@ -49,4 +43,4 @@ for action, elem in context:
         del elem.getparent()[0]
     # make sure you have no references to Element objects outside the loop
 
-sys.stdout.write("</mods:modsCollection>\n")
+sys.stdout.write("</collection>\n")
